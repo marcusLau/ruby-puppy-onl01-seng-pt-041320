2aad3164039d063c5d4ab505151e7efe9510e0ed
Marcus Lau <lau.marcus76@gmail.com>
2020-04-29 22:37:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,17 +16,17 @@ class Dog
   end
     
   
-  def all 
+  def self.all 
     @@all
   end
   
-  def print_all
+  def self.print_all
     @@all.each do |dog|
       puts dog.name
     end
   end
   
-  def @@clear_all 
+  def self.clear_all 
     @@all.clear
   end
   
